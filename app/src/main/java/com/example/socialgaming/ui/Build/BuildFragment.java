f6b694a25e389a25a384c44a4b5de8185411f6a2
umbyf117 <u.melpignano@campus.unimib.it>
2023-01-30 21:36:30 +0100

    roba

--- app/src/main/java/com/example/socialgaming/ui/Build/BuildFragment.java
+++ app/src/main/java/com/example/socialgaming/ui/Build/BuildFragment.java
@@ -9,14 +9,14 @@ import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.socialgaming.databinding.FragmentComponentsBinding;
+import com.example.socialgaming.databinding.FragmentBuildBinding;
 
 public class BuildFragment extends Fragment {
-    private FragmentComponentsBinding binding;
+    private FragmentBuildBinding binding;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         BuildViewModel bdvm = new ViewModelProvider(this).get(BuildViewModel.class);
-        binding = FragmentComponentsBinding.inflate(inflater, container, false);
+        binding = FragmentBuildBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         return root;
