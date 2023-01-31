e5b7fd57e5b1e2040384ba430b94cd0cd9bece1b
MattiaBiancini <m.biancini1@campus.unimib.it>
2023-01-31 21:01:39 +0100

    Create JSON Parser, Class File for Build and Components, BuildUtils for Component Creation and Management, Wrapper Class to REST GET

--- app/src/main/java/com/example/socialgaming/ui/Search/SearchFragment.java
+++ app/src/main/java/com/example/socialgaming/ui/Search/SearchFragment.java
@@ -21,7 +21,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.socialgaming.R;
-import com.example.socialgaming.api.ComponentsFetchr;
+import com.example.socialgaming.api.ComponentsFetcher;
 
 import org.json.JSONArray;
 import org.json.JSONException;
