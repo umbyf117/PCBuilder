9652a7e76baf4a60f96924931be85c622b3cc2e2
claudiofeneri <c.feneri@campus.unimib.it>
2023-02-08 19:07:46 +0100

    Build details pt1

--- app/src/main/java/com/example/socialgaming/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/socialgaming/ui/home/HomeFragment.java
@@ -271,7 +271,28 @@ public class HomeFragment extends Fragment implements IUserCallback, IBuildCallb
 
     public void createpopupwindow(View templateView, BuildFirestore b) {
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(LAYOUT_INFLATER_SERVICE);
-        View popUpView = inflater.inflate(R.layout.card_component, null);
+        View popUpView = inflater.inflate(R.layout.popupbuild, null);
+
+        TextView nameBuild = popUpView.findViewById(R.id.buildName);
+        nameBuild.setText(b.getName());
+        TextView motherboard = popUpView.findViewById(R.id.info1);
+        motherboard.setText("Motherboard: " + b.getBoardTitle());
+        TextView cpu = popUpView.findViewById(R.id.info2);
+        cpu.setText("CPU: " + b.getCpuTitle());
+        TextView ram = popUpView.findViewById(R.id.info3);
+        ram.setText("RAM: " + b.getRamsTitle());
+        TextView cpuFan = popUpView.findViewById(R.id.info4);
+        cpuFan.setText("CPU Fan: " + b.getFanTitle());
+        TextView gpu = popUpView.findViewById(R.id.info5);
+        gpu.setText("Graphic Card: " + b.getGpuTitle());
+        TextView storage = popUpView.findViewById(R.id.info6);
+        storage.setText("Storage: " + b.getMemoriesTitle());
+        TextView psu = popUpView.findViewById(R.id.info7);
+        psu.setText("Power Supply: " + b.getPsuTitle());
+        TextView mcase = popUpView.findViewById(R.id.info8);
+        mcase.setText("Case: " + b.getHouseTitle());
+        TextView price = popUpView.findViewById(R.id.price);
+        price.setText();
 
         int width = ViewGroup.LayoutParams.WRAP_CONTENT;
         int height = ViewGroup.LayoutParams.WRAP_CONTENT;
