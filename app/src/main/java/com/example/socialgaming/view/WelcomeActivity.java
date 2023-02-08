a798cac57c1cf0417be275b3cb96adc9368a5bef
umbyf117 <u.melpignano@campus.unimib.it>
2023-02-08 23:07:19 +0100

    Splash Screen

---
+++ app/src/main/java/com/example/socialgaming/view/WelcomeActivity.java
@@ -0,0 +1,28 @@
+package com.example.socialgaming.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.socialgaming.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        int welcomeDuration = 3000; // 5 secondi
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        }, welcomeDuration);
+    }
+}
